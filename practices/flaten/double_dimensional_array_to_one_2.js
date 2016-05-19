4b236bf6a8440332ecb4c5979ba89681471db302
Fiooona <845559655@qq.com>
2016-05-19 16:08:04 +0800

    fix

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function double_to_one(collection) {
-  var s=double_to_one1(collection)
+  var s=collection.join(",").split(",");
+  for(var j=0;j< s.length;j++){
+    s[j]=parseInt(s[j]);
+  }
+
   var r=new Array();
   label:for(var i = 0, n = s.length; i < n; i++) {
     for(var x = 0, y = r.length; x < y; x++) {
